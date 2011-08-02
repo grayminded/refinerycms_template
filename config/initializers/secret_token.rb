b848d764421438a7b38f6c9bad7fa08434ad828f
6hoursdaily <jerrypascua@gmail.com>
2011-08-02 20:28:00 +0800

    "working commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefinerycmsTemplate::Application.config.secret_token = 'a63e2551e2de5c1bcf29a8e237e771da9373f9bf198e0936e23c8a91e7c551c188a89469d34258fc1c72f345fe053371a29626d73ec68482ad300aef54b07cc3'
